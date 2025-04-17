d3dfa7647c4101732284bf81d3f7fac5ef2ba36f
Jason Penny <jason.penny@braze.com>
2025-04-17 16:57:39 -0400

    Backport https://github.com/petergoldstein/dalli/pull/936

--- lib/dalli/socket.rb
+++ lib/dalli/socket.rb
@@ -28,7 +28,7 @@ begin
     alias :write :kgio_write
 
     def readfull(count)
-      value = String.new('')
+      value = String.new(capacity: count + 1)
       while true
         value << kgio_read!(count - value.bytesize)
         break if value.bytesize == count
@@ -95,7 +95,7 @@ rescue LoadError
   module Dalli::Server::KSocket
     module InstanceMethods
       def readfull(count)
-        value = String.new('')
+        value = String.new(capacity: count + 1)
         begin
           while true
             value << read_nonblock(count - value.bytesize)
