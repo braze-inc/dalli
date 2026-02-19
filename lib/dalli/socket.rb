6b4c41677222de5974a2dd73e8612da0567036c6
Matt Dick <matthew.dick@braze.com>
2026-02-19 13:31:56 -0800

    Reduce GVL contention with non-blocking socket connections and non-blocking writes

--- lib/dalli/socket.rb
+++ lib/dalli/socket.rb
@@ -21,6 +21,22 @@ module Dalli
         value
       end
 
+      def writefull(bytes)
+        offset = 0
+        while offset < bytes.bytesize
+          chunk = offset == 0 ? bytes : bytes.byteslice(offset..-1)
+          result = write_nonblock(chunk, exception: false)
+          if result == :wait_writable
+            raise Timeout::Error, "IO timeout: #{safe_options.inspect}" unless IO.select(nil, [self], nil, options[:socket_timeout])
+          elsif result == :wait_readable
+            raise Timeout::Error, "IO timeout: #{safe_options.inspect}" unless IO.select([self], nil, nil, options[:socket_timeout])
+          else
+            offset += result
+          end
+        end
+        offset
+      end
+
       def read_available
         value = +""
         loop do
@@ -43,35 +59,68 @@ module Dalli
       end
     end
 
-    class TCP < TCPSocket
+    class TCP < ::Socket
       include Dalli::Socket::InstanceMethods
       attr_accessor :options, :server
 
       def self.open(host, port, server, options = {})
-        Timeout.timeout(options[:socket_timeout]) do
-          sock = new(host, port)
-          sock.options = {host: host, port: port}.merge(options)
-          sock.server = server
-          sock.setsockopt(::Socket::IPPROTO_TCP, ::Socket::TCP_NODELAY, true)
-          sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_KEEPALIVE, true) if options[:keepalive]
-          sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_RCVBUF, options[:rcvbuf]) if options[:rcvbuf]
-          sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_SNDBUF, options[:sndbuf]) if options[:sndbuf]
-          sock
+        addr_info = ::Socket.getaddrinfo(host, nil, ::Socket::AF_UNSPEC, ::Socket::SOCK_STREAM)
+        ai = addr_info.first
+        sock = new(ai[4], ::Socket::SOCK_STREAM, 0)
+
+        sock.setsockopt(::Socket::IPPROTO_TCP, ::Socket::TCP_NODELAY, true)
+        sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_KEEPALIVE, true) if options[:keepalive]
+        sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_RCVBUF, options[:rcvbuf]) if options[:rcvbuf]
+        sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_SNDBUF, options[:sndbuf]) if options[:sndbuf]
+
+        sockaddr = ::Socket.pack_sockaddr_in(port, ai[3])
+        result = sock.connect_nonblock(sockaddr, exception: false)
+        if result == :wait_writable
+          unless IO.select(nil, [sock], nil, options[:socket_timeout])
+            raise Timeout::Error, "Connection timeout: #{host}:#{port}"
+          end
+          begin
+            sock.connect_nonblock(sockaddr)
+          rescue Errno::EISCONN
+            # already connected
+          end
         end
+
+        sock.options = { host: host, port: port }.merge(options)
+        sock.server = server
+        sock
+      rescue
+        sock&.close rescue nil
+        raise
       end
     end
 
-    class UNIX < UNIXSocket
+    class UNIX < ::Socket
       include Dalli::Socket::InstanceMethods
       attr_accessor :options, :server
 
       def self.open(path, server, options = {})
-        Timeout.timeout(options[:socket_timeout]) do
-          sock = new(path)
-          sock.options = {path: path}.merge(options)
-          sock.server = server
-          sock
+        sock = new(::Socket::AF_UNIX, ::Socket::SOCK_STREAM, 0)
+        sockaddr = ::Socket.pack_sockaddr_un(path)
+
+        result = sock.connect_nonblock(sockaddr, exception: false)
+        if result == :wait_writable
+          unless IO.select(nil, [sock], nil, options[:socket_timeout])
+            raise Timeout::Error, "Connection timeout: #{path}"
+          end
+          begin
+            sock.connect_nonblock(sockaddr)
+          rescue Errno::EISCONN
+            # already connected
+          end
         end
+
+        sock.options = { path: path }.merge(options)
+        sock.server = server
+        sock
+      rescue
+        sock&.close rescue nil
+        raise
       end
     end
   end
