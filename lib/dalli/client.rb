e271c2e1dc86fd46121f823c273b5d8b805bad51
Matt Dick <matthew.dick@braze.com>
2026-02-27 12:51:22 -0800

    Simplify get_multi timeout handling after socket hardening.
    Remove the extra timeout rescue/close path from get_multi_yielder now that timeout cleanup is handled at the socket/protocol layers.

--- lib/dalli/client.rb
+++ lib/dalli/client.rb
@@ -433,7 +433,6 @@ module Dalli
       perform do
         return {} if keys.empty?
         ring.lock do
-          groups = {}
           begin
             groups = groups_for_keys(keys)
             if unfound_keys = groups.delete(nil)
@@ -485,9 +484,6 @@ module Dalli
                 end
               end
             end
-          rescue Timeout::Error
-            groups.each_key(&:close)
-            raise
           end
         end
       end
